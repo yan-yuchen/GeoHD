bd88f6670c5b4b548f1279a6a1efe359721fcaa3
chenchen <2436503722@qq.com>
2024-04-08 03:28:15 +0800

    v0.1.6..

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='GeoHD',
-    version='0.1.5',
+    version='0.1.6',
     description='A Python toolkit for geospatial hotspot detection, Avisualization, and analysis using urban data',
     author='Yuchen Yan',
     author_email='ycyan001@gmail.com',
